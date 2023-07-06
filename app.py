6329d1d58a7208a4df767ba0a8acc4b06d36a538
Hasnain Gandhi <56540038+hasnaingandhi@users.noreply.github.com>
2023-07-06 10:24:09 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import random
 import os
 import datetime
+import openai
+
 from werkzeug.datastructures import MultiDict
 import matplotlib.pyplot as plt
 from transformers import GPT2TokenizerFast
@@ -13,8 +15,8 @@ from langchain.llms import OpenAI
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-os.environ["OPENAI_API_KEY"] = "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"
-#openai.api_key = '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'
+openai.api_key = os.environ.get('Api_Key')
+
 conversation = []
 
 @app.route('/')
@@ -66,36 +68,10 @@ def chatbot_response(user_input):
         length_function = count_tokens,
     )
 
-    #chunks = text_splitter.create_documents([textL])
     embeddings = OpenAIEmbeddings()
     db = FAISS.from_documents(text_splitter.create_documents([text]),embeddings)
     chain = load_qa_chain(OpenAI(temperature=0,model_name='text-davinci-003'), chain_type="stuff")
     query = user_input
-    '''
-    prompt = f"""
-        Your task is to find the answer of the query by using the given context.
-        Steps to follow: 
-        1. Use the given file to answer and ignore any hallucination as well as outside data except the given context.
-
-        Information is given in the following format 
-        Question:
-        ```
-        question here
-        ```
-
-        answer here
-        ```
-
-        Question:
-        ```
-        {query}
-
-        ```
-
-        Answer
-    """
-    '''
-    prompt = query
     
     docs = db.similarity_search(query)
 
